400e69cc74a5a397e89f60a25c42969848380148
emiliano231 <143216851+emiliano231@users.noreply.github.com>
2023-09-11 11:45:13 -0700

    Add files via upload

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class HELLOWORLD here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DataTypes
+{
+    int i;
+    double f = 2.5;
+    boolean flag = true;
+    
+    
+    public static void main(String[] args)
+    {
+        System.out.print("ABC 123");
+        System.out.println(" mic check");
+        System.out.print("good Bye People");
+    }
+}
+
